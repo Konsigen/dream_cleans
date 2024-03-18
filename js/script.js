1980cad292bce2719f2c0df5553f56485530bd02
b7sj3o <vitalikfedytnyk@gmail.com>
2024-03-18 14:48:57 +0200

    s

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ rangeInput.addEventListener('input', rangeInputSlider);
 document.addEventListener('DOMContentLoaded', () => {
     setTimeout(function(){
         preloader.style.display = "none";
-    }, 20);
+    }, 2000);
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
